a3f258830f99646e67741b48cae5b2c6b3a6c55d
Adarsh Prakash <87135900+Adarshsharath@users.noreply.github.com>
2026-07-15 17:08:06 +0530

    Time: 0 ms (100%), Space: 19.3 MB (27.54%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+        i = 0
+        j = 0
+        k = 0
+        p = 0
+        first = False
+        second = False
+        third = False
+        while i + 1 < len(nums) and nums[i] < nums[i+1]:
+            while i + 1 < len(nums) and (nums[i] < nums[i+1]):
+                i+=1
+                j+=1
+            if j > 0:
+                first = True
+            while i + 1 < len(nums) and (nums[i]>nums[i+1]):
+                i+=1
+                k+=1
+            if k > 0:
+                second = True
+            while i + 1 < len(nums) and (nums[i]<nums[i+1]):
+                i+=1
+                p+=1
+            if p > 0:
+                third = True 
+        return first and second and third and i == len(nums)-1

681790d09e143eada93b45b27667cbecd05e38bb
Adarsh Prakash <87135900+Adarshsharath@users.noreply.github.com>
2026-07-15 17:10:15 +0530

    Time: 3 ms (20.09%), Space: 19.5 MB (5.76%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.py
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -7,20 +7,20 @@ class Solution:
         first = False
         second = False
         third = False
-        while i + 1 < len(nums) and nums[i] < nums[i+1]:
-            while i + 1 < len(nums) and (nums[i] < nums[i+1]):
-                i+=1
-                j+=1
-            if j > 0:
-                first = True
-            while i + 1 < len(nums) and (nums[i]>nums[i+1]):
-                i+=1
-                k+=1
-            if k > 0:
-                second = True
-            while i + 1 < len(nums) and (nums[i]<nums[i+1]):
-                i+=1
-                p+=1
-            if p > 0:
-                third = True 
+
+        while i + 1 < len(nums) and (nums[i] < nums[i+1]):
+            i+=1
+            j+=1
+        if j > 0:
+            first = True
+        while i + 1 < len(nums) and (nums[i]>nums[i+1]):
+            i+=1
+            k+=1
+        if k > 0:
+            second = True
+        while i + 1 < len(nums) and (nums[i]<nums[i+1]):
+            i+=1
+            p+=1
+        if p > 0:
+            third = True 
         return first and second and third and i == len(nums)-1

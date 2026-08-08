732193676dae64c94f00a93c1ede7253ccdf630c
Adarsh Prakash <87135900+Adarshsharath@users.noreply.github.com>
2026-08-08 14:23:22 +0530

    Time: 54 ms (70.79%), Space: 23.9 MB (86.91%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,18 +9,69 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // ListNode* curA = headA;
+        // ListNode* curB = headB;
+
+        // while (curA!= NULL){
+        //     curB = headB;
+        //     while(curB!=NULL){
+        //         if (curA == curB){
+        //             return curA;
+        //         }
+        //         curB = curB->next;
+        //     }
+        //     curA= curA->next;
+        // }
+        // return NULL;
         ListNode* curA = headA;
         ListNode* curB = headB;
+        int lenA = 0;
+        int lenB = 0;
+        while(curA!=NULL){
+            lenA++;
+            curA=curA->next;
+        }
+        while(curB!=NULL){
+            lenB++;
+            curB=curB->next;
+        }
+        int sm = 0;
+        curA = headA;
+        curB = headB;
 
-        while (curA!= NULL){
-            curB = headB;
-            while(curB!=NULL){
-                if (curA == curB){
+        if (lenA > lenB){
+            sm = lenA - lenB;
+            while(sm){
+                if (curA!=NULL){
+                    curA = curA->next;
+                }
+                sm--;
+            }
+            while(curA!=NULL){
+                
+                if(curA==curB){
                     return curA;
                 }
-                curB = curB->next;
+                curA= curA->next;
+                curB= curB->next;
             }
-            curA= curA->next;
+        }
+        else{
+            sm = lenB - lenA;
+            while(sm){
+                if (curB!=NULL){
+                    curB = curB->next;
+                }
+                sm--;
+            }
+            while(curB!=NULL){
+                
+                if(curA==curB){
+                    return curA;
+                }
+                curA= curA->next;
+                curB= curB->next;
+            }
         }
         return NULL;
     }

1840eb32d3de3442a4eeab575aecdc9f54a0d5b2
Adarsh Prakash <87135900+Adarshsharath@users.noreply.github.com>
2026-09-10 21:52:57 +0530

    Time: 45 ms (88.27%), Space: 19.5 MB (69.46%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -4,30 +4,52 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def averageOfSubtree(self, root: TreeNode) -> int:
+#         def inorder(root,ls):
+#             if not root:
+#                 return
+#             inorder(root.left,ls)
+#             ls.append(root.val)
+#             inorder(root.right,ls)
+#             return ls
+#         count = [0]
+#         def fun(root):
+#             if not root:
+#                 return
+            
+#             ls = inorder(root,[])
+#             S = sum(ls)//len(ls)
+#             if root.val == S:
+#                 count[0] += 1
+#             fun(root.left)
+#             fun(root.right)
+
+#         fun(root)
+#         return count[0]
+
 class Solution:
     def averageOfSubtree(self, root: TreeNode) -> int:
-        def inorder(root,ls):
-            if not root:
-                return
-            inorder(root.left,ls)
-            ls.append(root.val)
-            inorder(root.right,ls)
-            return ls
+
         count = [0]
-        def fun(root):
+
+        def dfs(root):
             if not root:
-                return
-            
-            ls = inorder(root,[])
-            S = sum(ls)//len(ls)
-            if root.val == S:
+                return 0, 0
+
+            left_sum, left_count = dfs(root.left)
+            right_sum, right_count = dfs(root.right)
+
+            total_sum = left_sum + right_sum + root.val
+            total_count = left_count + right_count + 1
+
+            if root.val == total_sum // total_count:
                 count[0] += 1
-            fun(root.left)
-            fun(root.right)
 
-        fun(root)
+            return total_sum, total_count
+
+        dfs(root)
         return count[0]
-
 
 
 

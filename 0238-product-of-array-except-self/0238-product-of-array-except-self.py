eedc26ac23e1fc01e7469b765c9d2d0aae57edb1
Adarsh Prakash <87135900+Adarshsharath@users.noreply.github.com>
2026-09-12 22:45:18 +0530

    Time: 33 ms (27.49%), Space: 27.9 MB (7.45%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,34 +1,52 @@
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         p = 1
+#         pre =[0]*len(nums)
+#         pre[0] = 0
+#         pre[1] = nums[0]
+#         p = pre[1]
+#         for i in range(2,len(nums)):
+#             p = p * nums[i-1]
+#             pre[i] = p
+        
+
+#         suf =[0]*len(nums)
+#         suf[len(nums)-1] = 0
+#         suf[len(nums)-2] = nums[-1]
+#         p = nums[-1]
+#         for i in range(len(nums)-3,-1,-1):
+#             p = p*nums[i+1]
+#             suf[i] = p
+        
+#         ans = []
+#         for i in range(len(nums)):
+#             if i == 0:
+#                 ans.append(suf[i])
+#                 continue
+#             if i == len(nums)-1:
+#                 ans.append(pre[i])
+#                 continue
+#             ans.append(pre[i]*suf[i])
+#         return ans
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        p = 1
-        pre =[0]*len(nums)
-        pre[0] = 0
-        pre[1] = nums[0]
-        p = pre[1]
-        for i in range(2,len(nums)):
-            p = p * nums[i-1]
-            pre[i] = p
-        
+        n = len(nums)
 
-        suf =[0]*len(nums)
-        suf[len(nums)-1] = 0
-        suf[len(nums)-2] = nums[-1]
-        p = nums[-1]
-        for i in range(len(nums)-3,-1,-1):
-            p = p*nums[i+1]
-            suf[i] = p
-        
+        pre = [1] * n
+        suf = [1] * n
+
+        for i in range(1, n):
+            pre[i] = pre[i - 1] * nums[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            suf[i] = suf[i + 1] * nums[i + 1]
+
         ans = []
-        for i in range(len(nums)):
-            if i == 0:
-                ans.append(suf[i])
-                continue
-            if i == len(nums)-1:
-                ans.append(pre[i])
-                continue
-            ans.append(pre[i]*suf[i])
-        return ans
 
-            
+        for i in range(n):
+            ans.append(pre[i] * suf[i])
+
+        return ans
             
 
